573eca9cf1deb4d4533df690ef520b36a742321b
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-03-15 19:14:02 +0100

    upload icons

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ import sys
 import os
 from cx_Freeze import setup, Executable
 
-files = ['models.py','ui_form.py','AANOTHERTAG.TTF', 'Icons.qrc', 'Icons_rc.py','form.ui']
+files = ['models.py','ui_form.py','AANOTHERTAG.TTF', 'Icons.qrc', 'Icons_rc.py','form.ui','app_icon.ico']
 
 target = Executable(script='main.py',
-                    base = 'Win32GUI')
+                    base = 'Win32GUI',
+                    icon = 'app_icon.ico')
 
 setup(
     name='M0W1Z',
-    version='0.9.3',
+    version='1.0.3',
     description="A System-Monitor for Tasks, Battery, System-Information, CPU, Memory, Storage.",
     author="S3R43o3",
     options={'build_exe': {'include_files': files}},
